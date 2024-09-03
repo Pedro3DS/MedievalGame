4778f39e6b9e363dd03cda15009c043c15f5b52e
Pedroca <132005678+Pedro3DS@users.noreply.github.com>
2024-09-02 20:10:02 -0400

    update

--- Assets/Scripts/StartGame.cs
+++ Assets/Scripts/StartGame.cs
@@ -5,14 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class StartGame : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetKey("Start") && SceneManager.GetActiveScene().name == "StartScene")
+        {
+            StartCatGame();
+        }
+        if (Input.GetKey("Exit"))
+        {
+            ExitGame();
+        }
+    }
     public void StartCatGame()
     {
+
         SceneManager.LoadScene("SampleScene");
     }
 
     public void RestartGame()
     {
         SceneManager.LoadScene("StartScene");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
     }
 
 }
